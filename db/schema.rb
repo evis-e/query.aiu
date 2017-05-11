ffb2e642540f91c45045da1db16f4ebed1af2fa4
evis-e <evis6174@gmail.com>
2017-05-11 11:33:16 +0300

    Generated Post model.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170429101030) do
+ActiveRecord::Schema.define(version: 20170511083042) do
+
+  create_table "posts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.text "title"
+    t.text "content"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id", "created_at"], name: "index_posts_on_user_id_and_created_at"
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "username"
@@ -20,4 +30,5 @@ ActiveRecord::Schema.define(version: 20170429101030) do
     t.string "password_digest"
   end
 
+  add_foreign_key "posts", "users"
 end
