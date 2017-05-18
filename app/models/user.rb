84e15b41ac88febdd7201c9ccc56d7869828b523
evis-e <evis6174@gmail.com>
2017-05-18 13:57:54 +0300

    Changed the home feed to all posts except the ones posted by the current user. Redesigned the user list.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,20 @@
 class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   before_save { self.email = email.downcase }
-  validates :username, presence: true, length: { maximum: 50 }
+  validates :username, presence: true, length: { maximum: 50 },
+                       uniqueness: { case_sensitive: false }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
   has_secure_password
 
-  # Defines a proto-feed.
-  # See "Following users" for the full implementation.
-  def feed
+  def user_posts
     Post.where("user_id = ?", id)
+  end
+
+  def feed
+    Post.where.not("user_id = ?", id)
   end
 
 end
